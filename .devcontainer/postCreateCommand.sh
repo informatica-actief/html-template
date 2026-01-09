031e8256fa688f20b724d3b02fe8894606626a35
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2026-01-09 14:40:29 +0000

    fix bugs in devcontainer config

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install Python packages
-pip install pygame-ce
-pip install coderius-play
-
 # Configure git settings
 git config pull.rebase false # needs to be configured before first pull or merge to prevent error
 git config merge.ours.driver true # configure git to use 'ours' merge strategy by default, this is not very git-like, but makes it easier voor studenten
